ed4cd0c243ef873a8a8e6da952cd2a2f1dd7fcab
sonal0205 <94561078+sonal0205@users.noreply.github.com>
2023-02-18 14:29:58 +0530

    Create file.cpp

---
+++ file.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, This is Jenkins Pipeline\n";
+  cout << "PES1UG20CS431 has successfully built and run\n";
+  return 0;
+}
